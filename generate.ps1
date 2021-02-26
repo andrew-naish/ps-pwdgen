a91939ea1b47cc6e30ccbcf963ec45aed0bf6d4e
Andrew Naish <andy.naish1@gmail.com>
2021-02-26 20:14:40 +0000

    rewrote generate.ps1
    Also added feature to define specific wordlist to use.

--- generate.ps1
+++ generate.ps1
@@ -2,13 +2,10 @@
     [switch]$NoInfo,
     $WordlistDirectory=".\wordlists",
     $Count=1,
-    $PasswordMask="{word}#{int}"
+    $PasswordMask="{wl:colours}{wl:random}#{int}"
 )
 
-$seperators=@("@","#")
-
-$idx_formatters = @()
-$using_WordLists = @{}
+$formatters = @()
 $rtnpwdList = @()
 
 $vowel_upper = @('A', 'E', 'I', 'O', 'U')
@@ -16,81 +13,113 @@ $vowel_lower = @('a', 'e', 'i', 'o', 'u')
 $consonant_lower = @("b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z")
 $consonant_upper = @('B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z')
 
-## Locate formatters 
+# get wordlists
+$wordlists_all = Get-ChildItem -Path ($wordlistDirectory | Resolve-Path).Path -Filter "*.txt"
+$wordlists_available = {($wordlists_all.BaseName)}.Invoke() # converting it to a System.Collections.ObjectModel.Collection - this allows the use of .Remove()
 
-$count_wordlists  = ([regex]::Matches($PasswordMask, "{word}" )).count
+#  funct
+function GetWordFromWordlist($WordlistName) {
+    $wl_file = $wordlists_all | Where-Object {$_.basename -eq "$WordlistName"}
 
-[regex]::Matches($PasswordMask, "{(\w)+}|(.)" ) | ForEach-Object {
-    
-    $row = New-Object PSObject -Property @{
-        "Index" = $_.Index
-        "Value" = $_.Value
+    # early ticket home if not found
+    if( $null -eq $wl_file ) { 
+        throw "404 wordlist not found - Check there's a wordlist named '$WordlistName' in the wordlist directory"
     }
 
-    $idx_formatters += $row
+    return $wl_file | Get-Content | Get-Random
+}
+
+function ParseExplicitWordlistMask ($ExplicitWordlistMask) {
+    $wl_match = [regex]::Match($ExplicitWordlistMask, "{wl:(\w+)}" )
+    return $wl_match.Groups[1].Value
+
+    # if the name of the wordlist is invalid
+    #   we'll catch it later on when we try to get a word from it 
+}
+
+## Main
+
+# parse the password mask
+[regex]::Matches($PasswordMask, "{((wl:)?\w)+}|(.)" ) | ForEach-Object {
+    
+    $new_formatter = New-Object PSObject -Property @{
+        "index" = $_.Index
+        "value" = $_.Value
+        "wordlist" = "" # this will be populated later on for each word-related formatter
+    }
+
+    $formatters += $new_formatter
 
 }
 
-## Choose random wordlists 
+$wordy_formatters = $formatters | Where-Object {$_.Value -like "{wl:*}"}
 
-$allWordLists = Get-ChildItem -Path ($wordlistDirectory | Resolve-Path).Path -Filter "*.txt"
-$usedIndex = @(); for ($i = 0; $i -lt $count_wordlists; $i++) {
-    # Dowhile to prevent same index being chosen twice
-    do {$rnd = Get-Random -Minimum 0 -Maximum $allWordLists.Length}
-    while ($usedIndex -contains $rnd)
-    $usedIndex += $rnd
-        
-    # Add to using
-    $using_WordLists.Add(
-        $($allWordLists[$rnd].Name),
-        [array](Get-Content $allWordLists[$rnd].FullName)
-    )
+# check if too many wordlists
+if ([int]($wordy_formatters| Measure-Object).Count -gt $wordlists_available.Count) {
+    throw "Too many formatters which will require a random wordlist. Either add more wordlists or downgrade your formatter game!"
 }
 
-if (!$NoInfo) {
-    # WRITE
+# assign a wordlist to wordy formatters
+foreach ($formatter in $wordy_formatters) {
+
+    # random wordlist formatter
+    if ($formatter.value -eq "{wl:random}") {
+        $chosen_wordlist = $wordlists_available | Get-Random
+    }
+
+    # specific wordlist formatter
+    else {
+        $chosen_wordlist = ParseExplicitWordlistMask -ExplicitWordlistMask "$($formatter.Value)"
+    }
+
+    # allocate and mark as unavailable
+    $formatter.wordlist = "$chosen_wordlist"
+    $wordlists_available.Remove("$chosen_wordlist") | Out-Null
+
+}
+
+# to nag or not to nag, that is the question
+if (-not $NoInfo) {
+
     Write-Host "Generating passwords using pattern: " -NoNewline
     Write-Host "$PasswordMask" -ForegroundColor Yellow -NoNewline
     Write-Host ", with wordlist(s): " -NoNewline
-    Write-Host "$($using_WordLists.Keys -join ',')" -ForegroundColor Yellow
+    Write-Host "$( ($($formatters | Sort-Object Index).wordlist | Where-Object {$_ -ne ''}) -join ',')" -ForegroundColor Yellow
+
 }
 
-## Main generate password loop
-
+# do the thing
 for ($i = 0; $i -lt $Count; $i++) {
 
+    # each element of the password will go here, we'll join it in the end
     $pw = @()
     
-    foreach ($item in $idx_formatters | Sort-Object Index) {
+    foreach ($item in $formatters | Sort-Object Index) {
 
-        switch ($item.Value) {
+        switch -Wildcard ($item.Value) {
             
-            '{word}'      {$pw += $using_WordLists.Values | Get-Random; break}
-            '{seperator}' {$pw += $seperators | Get-Random; break}
-            '{int}'       {$pw += Get-Random -Minimum 1000 -Maximum 9999; break}
-            '{vowel}'     {$pw += $vowel_lower | Get-Random; break}
-            '{conso}'     {$pw += $consonant_lower | Get-Random; break}
-            '{vowelUpper}'     {$pw += $vowel_upper | Get-Random; break}
-            '{consoUpper}'     {$pw += $consonant_upper | Get-Random; break}
+            '{wl:random}'       {$pw += GetWordFromWordlist($($item.wordlist)); break}
+            '{wl:*}'            {$pw += GetWordFromWordlist($($item.wordlist)); break}
+            '{int}'             {$pw += Get-Random -Minimum 1000 -Maximum 9999; break}
+            '{vowel}'           {$pw += $vowel_lower | Get-Random; break}
+            '{consonant_lower}' {$pw += $consonant_lower | Get-Random; break}
+            '{vowel_upper}'     {$pw += $vowel_upper | Get-Random; break}
+            '{consonant_upper}' {$pw += $consonant_upper | Get-Random; break}
 
             default {$pw += $item.Value}
 
         }
-
     }
 
-    $rtnpwdList += (Get-Culture).TextInfo.ToTitleCase("$(-join $pw)") # to TitleCase
-
+    $rtnpwdList += -join $pw
 }
 
-## Return to sender
+## Output
 
 if ($rtnpwdList.Count -eq 1) {
-
     Write-Output $rtnpwdList[0]
+    } 
 
-} else {
-
+else {
     return $rtnpwdList
-
-}
+    }
