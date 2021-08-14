a1ac6e1c2363cf489fb615adc0221b4ccd056fb6
Andrew Naish <andy.naish1@gmail.com>
2021-08-14 04:29:10 +0100

    tbh I just went mad this day..

--- tools/clean-wordlist.ps1
+++ tools/clean-wordlist.ps1
@@ -1,10 +1,11 @@
 ﻿## Init
-
-param(
+Param(
 
     # Path to a specific wordlist.
-    [Parameter(Mandatory = $true, HelpMessage="Path to a specific wordlist.")]
-    [String] $Wordlist,
+    #[Parameter(Mandatory = $true, HelpMessage="Path to a specific wordlist.")]
+    #[ArgumentCompleter( { param($cmd, $param, $wordToComplete) $((Get-ChildItem -Path "..\wordlists\plaintext\").BaseName) -like "$wordToComplete*" } )]
+    #[ValidateScript( { $_ -in $((Get-ChildItem -Path "..\wordlists\plaintext\").BaseName) })]
+    #[String[]] $Wordlists,
 
     # Check word length, if less than defined min or more than defined max, remove it.
     #   If this param is used, DynamicParams will be added in the block below.
@@ -59,17 +60,18 @@ DynamicParam {
     if ($BySentiment) {
 
         # Define: SentimentReport
-            $bysentiment_sentimentrpt_param_attr = New-Object -Type System.Management.Automation.ParameterAttribute
-            $bysentiment_sentimentrpt_param_attr.Mandatory = $true
+            $bysentiment_sntthreshold_param_attr = New-Object -Type System.Management.Automation.ParameterAttribute
+            $bysentiment_sntthreshold_param_attr.Mandatory = $false
 
-            $bysentiment_sentimentrpt_param_attrcol = New-Object -Type System.Collections.ObjectModel.Collection[System.Attribute]
-            $bysentiment_sentimentrpt_param_attrcol.Add($bysentiment_sentimentrpt_param_attr)
+            $bysentiment_sntthreshold_param_attrcol = New-Object -Type System.Collections.ObjectModel.Collection[System.Attribute]
+            $bysentiment_sntthreshold_param_attrcol.Add($bysentiment_sntthreshold_param_attr)
 
             # create parameter object
-            $bysentiment_sentimentrpt_param = New-Object -Type System.Management.Automation.RuntimeDefinedParameter("SentimentReport", [String], $bysentiment_sentimentrpt_param_attrcol)
+            $bysentiment_sntthreshold_param = New-Object -Type System.Management.Automation.RuntimeDefinedParameter("SentimentThreshold", [Int], $bysentiment_sntthreshold_param_attrcol)
+            $bysentiment_sntthreshold_param.Value = 1 # default value
 
             # commit
-            $param_dictionary.Add('SentimentReport', $bysentiment_sentimentrpt_param)
+            $param_dictionary.Add('SentimentThreshold', $bysentiment_sntthreshold_param)
     
     }
 
@@ -80,36 +82,123 @@ DynamicParam {
 
 Begin {
 
-    Read-Host "PAUSE"
+    # import threadjob module
+    Import-Module "..\dependancies\ThreadJob\ThreadJob.psd1"
+
+    # TODO: import sqllite3.dll
+
+    # funct
+    function MessageAndContinue($Word, $Message) {
+        Write-Output "Removing word: $word, Reason: $Message"
+        continue
+    }
+
+    # var
+    $wordlists_dir = "..\wordlists"
+    $wordlists_plaintext_dir = "$wordlists_dir\plaintext"
+    $wordlists_plaintext_backups_dir = "$wordlists_plaintext_dir\backups"
+
+    # handle dynamic params
+    if ( $ByLength ) {
+        $acceptable_range = ($param_dictionary.MinLength.Value)..($param_dictionary.MaxLength.Value)
+    }
+    if ( $BySentiment ) {
+        $sentiment_threshold = $param_dictionary.SentimentThreshold.Value
+        $sentiment_map = @{
+            "NEGATIVE" = 0
+            "MIXED" = 1
+            "NEUTRAL" = 2
+            "POSITIVE" = 3
+        }
+    }
 
 }
 
+## Main
+
 Process {
 
+    ## backup current wordlists
+    Write-Output "Backing up current wordlists"
 
-    ## TODO: Backup current wordlists
+    # create dir if does not exist
+    if ( -not (Test-Path $wordlists_plaintext_backups_dir)) 
+    { New-Item -Path $wordlists_plaintext_backups_dir -ItemType Directory -Force | Out-Null }
 
-    ## TODO: Do the basic stuff 
-    #    - Convert to TitleCase
-    #    - Remove chars which are not [a-zA-Z]
+    # do the thing
+    Compress-Archive -Path "$wordlists_plaintext_dir\*.txt" -DestinationPath "$wordlists_plaintext_backups_dir\$(Get-Date -Format 'ddMMyyyy-HHmmss')_wordlistbackup.zip" | Out-Null
 
-    ## TODO: If ByLength
-    #    - Do stuff
+    ## itterate wordlists
+    Get-ChildItem -Path $wordlists_plaintext_dir -Filter "*.txt" | ForEach-Object {
 
-    ## TODO: If BySentiment
-    #    - Do stuff
+        $wordlist_current_name = $_.BaseName
+        Write-Output "On Wordlist: $wordlist_current_name"
 
-    Read-Host "PAUSE - Before Old Code"
-    ### Old code below ##
+        # import wordlist
+        $wordlist_current_content = Get-Content $_.FullName
 
-    # define backup dir
-    $backup_dir_path = ".\wordlist_backups\backup_$(Get-Date -Format "yyyyMMdd-HHmmss")" 
+        # remove current
+        #$_ | Remove-Item -Force
 
-    # create dir
-    if ( -not (Test-Path "$backup_dir_path")) {
-        New-Item -ItemType Directory -Name "$bkDirName" -Force | Out-Null
+        # prepare
+        $wordlist_new = New-Object -Type Collections.Generic.List[String]
+
+        # check if can sentiment
+        if ( $BySentiment ) {
+
+            $sentiment_report_path = "$wordlists_plaintext_dir\$($wordlist_current_name)_sentimentreport.csv"
+            $sentiment_report_present = $false
+
+            if ( Test-Path "$sentiment_report_path" ) {
+                $sentiment_report_present = $true
+                $sentiment_report = Import-Csv $sentiment_report_path
+            }
+
+        }
+
+        foreach ( $word in $wordlist_current_content ) {
+
+            # if empty, skip
+            if ( [string]::IsNullOrEmpty($word) ) {
+                continue
+            }
+
+            # by length
+            if ( $ByLength ) {
+                if ($word.Length -notin $acceptable_range) {
+                    MessageAndContinue $word "Length is not in acceptable range"
+                } 
+            }
+
+            # by sentiment
+            if ( $BySentiment -AND $sentiment_report_present ) {
+                $sentiment = ($sentiment_report | Where-Object {$_.Word -eq $word}).Sentiment
+                if ($sentiment_map[$sentiment] -lt $sentiment_threshold) {
+                    MessageAndContinue $word "Word is below sentiment threshold"
+                }
+            }
+
+            # TODO: convert to titlecase
+
+            # TODO: remove non a-z chars
+
+            # word is ok
+            #Write-Output "Word ok: $word"
+            $wordlist_new.Add($word)
+
+        }
+
+        # TODO: sort
+
+        # TODO: dump as text
+
+        # TODO: dump as .bin ?
+
+        Write-Output ""
     }
 
+
+    <#
     $titlecaseConverter = (Get-Culture).TextInfo
     $bkDirName = "wordlist_backup"
 
@@ -136,5 +225,7 @@ Process {
             }
             $stream.Close()
     }
+
+    #>
 
 }
